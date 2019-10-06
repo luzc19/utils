76336aa4e3a17f7b3293557977e663cbba4818c7
luzc19 <luzc19@163.com>
2019-10-06 10:55:02 +0800

    空格优化

--- updateObject.js
+++ updateObject.js
@@ -1,11 +1,11 @@
 function updateObject(target, source) {
-    if(typeof target == 'object' && typeof source == 'object' && source != null && target != null){
-        for(let key in target){
-            if(source[key]!==undefined){
+    if (typeof target == 'object' && typeof source == 'object' && source != null && target != null) {
+        for (let key in target) {
+            if (source[key] !== undefined) {
                 target[key] = source[key]
             }
         }
     }
     return target;
 }
-export default updateObject
+export default updateObject;
